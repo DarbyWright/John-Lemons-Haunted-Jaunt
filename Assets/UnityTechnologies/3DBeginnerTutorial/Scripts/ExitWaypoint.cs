19582480cc3d78b43be07eca707f84240b70307b
Darby Wright <wright.darby@yahoo.com>
2023-04-18 22:11:28 -0700

    added lerping effect to exit waypoint

--- Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/ExitWaypoint.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/ExitWaypoint.cs
@@ -8,29 +8,46 @@ public class ExitWaypoint : MonoBehaviour
     public Image img;
     public Transform target;
 
+    [SerializeField]
+    private float minSize;
+    [SerializeField]
+    private float maxSize;
+
+    private float initialDistance;
+    private float distanceToExit;
+    private float scaleFactor;
+    private float t;
+
+    private void Start()
+    {
+        initialDistance = Vector3.Magnitude(target.position - gameObject.transform.position);
+    }
+
     private void Update()
     {
-        float minX = (img.GetPixelAdjustedRect().width / 2);
-        float maxX = Screen.width - minX + 10f;
+        distanceToExit = Vector3.Magnitude(target.position - gameObject.transform.position);
+        t = distanceToExit / initialDistance;
+        scaleFactor = Mathf.LerpUnclamped(minSize, maxSize, t);
 
-        float minY = img.GetPixelAdjustedRect().height / 2;
+        img.rectTransform.localScale = new Vector3(scaleFactor, scaleFactor, 1);
+
+        float minX = (img.GetPixelAdjustedRect().width / 2) * 0.3f;
+        float maxX = Screen.width - minX;
+
+        float minY = (img.GetPixelAdjustedRect().height / 2) * 0.3f;
         float maxY = Screen.height - minY;
 
         Vector2 pos = Camera.main.WorldToScreenPoint(target.position);
-        // img.transform.position = Camera.main.WorldToScreenPoint(target.position);
 
-        // if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
-        // {
-        //     //Target is behind player
-        //     if (pos.x < Screen.width / 2)
-        //     {
-        //         pos.x = maxX;
-        //     }
-        //     else
-        //     {
-        //         pos.x = minX;
-        //     }
-        // }
+        // only render exit waypoint if youre facing away from exit
+        if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
+        {
+            img.enabled = true;
+        }
+        else
+        {
+            img.enabled = false;
+        }
 
         pos.x = Mathf.Clamp(pos.x, minX, maxX);
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
